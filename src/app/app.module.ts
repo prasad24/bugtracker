1af9a6b1347b404028d4c22d4cdad7f208122853
Prasad Subramanain <prasad2@hotmail.com>
2018-07-19 12:22:10 -0400

    Added component and set up routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AddComponent } from './components/add/add.component';
+import { EditComponent } from './components/edit/edit.component';
+import { ListComponent } from './components/list/list.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes = [
+  {path: 'add', component: AddComponent},
+  {path: 'edit', component: EditComponent},
+  {path: 'list', component: ListComponent},
+  {path: '', redirectTo: 'list', pathMatch:'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddComponent,
+    EditComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
